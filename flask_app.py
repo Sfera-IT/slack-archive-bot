cf179df7fe896b73e5d09d72ceba4e5a18d9adbb
Fabio Lombardo <lombax85@gmail.com>
2024-07-06 22:44:00 +0200

    [WIP] search V2

--- flask_app.py
+++ flask_app.py
@@ -336,12 +336,14 @@ def search_messages_V2():
         params.append('%' + channel_name + '%')
     
     if start_time:
-        sql += ' AND messages.timestamp >= ?'
-        params.append(start_time)
+        start_timestamp = datetime.fromisoformat(start_time.replace('Z', '+00:00')).timestamp()
+        sql += ' AND CAST(messages.timestamp AS FLOAT) >= ?'
+        params.append(start_timestamp)
     
     if end_time:
-        sql += ' AND messages.timestamp <= ?'
-        params.append(end_time)
+        end_timestamp = datetime.fromisoformat(end_time.replace('Z', '+00:00')).timestamp()
+        sql += ' AND CAST(messages.timestamp AS FLOAT) <= ?'
+        params.append(end_timestamp)
 
     sql += ' ORDER BY messages.timestamp DESC'
 
