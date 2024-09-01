685d52533a37dcbe6a90bd4d7345d4a12be4f978
Fabio Lombardo <lombax85@gmail.com>
2024-09-01 19:35:07 +0200

    [chores] clone db util

--- utilities/clone_db.sh
+++ utilities/clone_db.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-ssh root@sfera-docker "cp /srv/sferait/archive/slack.sqlite /srv/sferait/archive/slack_backup.sqlite"
-scp root@sfera-docker:/srv/sferait/archive/slack_backup.sqlite ./slack.sqlite
-ssh root@sfera-docker "rm /srv/sferait/archive/slack_backup.sqlite"
+ssh root@sfera-docker "cp /srv/sferait/archive/slack.sqlite /srv/sferait/archive/slack_backup.sqlite && gzip /srv/sferait/archive/slack_backup.sqlite"
+scp root@sfera-docker:/srv/sferait/archive/slack_backup.sqlite.gz ./slack.sqlite.gz
+ssh root@sfera-docker "rm /srv/sferait/archive/slack_backup.sqlite.gz"
 
